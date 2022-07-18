953d3aed3a18f4574429758e33e511fffb1a414f
Junior Pinheiro <junior.main7@gmail.com>
2022-07-18 12:40:27 -0300

    página principal quase ok com scroll top

---
+++ assets/scripts/index.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal({ reset: true });
+
+
+sr.reveal('.banner', { duration: 1000});
+sr.reveal('.sobre', {
+  rotate: {x: 0, y: 80, z:0},
+  duration: 1000
+});
+sr.reveal(".contato", {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 1000,
+});
+sr.reveal(".localizacao", {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 1000,
+});
