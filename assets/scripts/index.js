f12ccdc15643fb03002adcbca5e6b7fb5ba83b80
Junior Pinheiro <junior.main7@gmail.com>
2022-07-27 09:45:24 -0300

    feat: acomodações done. fix: home

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -14,3 +14,8 @@ sr.reveal(".localizacao", {
   rotate: { x: 0, y: 80, z: 0 },
   duration: 1000,
 });
+
+sr.reveal(".card-room", {
+  rotate: { x: 0, y: 80, z: 0 },
+  duration: 1000,
+});
---
+++ assets/scripts/rooms.js
@@ -0,0 +1,31 @@
+let solteiroBtn = document.querySelector('div.solteiro');
+let casalBtn = document.querySelector('div.casal');
+
+let v1 = document.querySelector('div.v1');
+let v2 = document.querySelector('div.v2');
+
+let imgSolteiro = document.getElementById("img-solteiro");
+let imgCasal = document.getElementById("img-casal");
+
+solteiroBtn.addEventListener('click', () => {
+  casalBtn.classList.remove('selected');
+  solteiroBtn.classList.add('selected');
+
+  v1.textContent = "R$ 60,00";
+  v2.textContent = "R$ 70,00";
+
+  imgSolteiro.src = "./assets/imgs/room1.png";
+  imgCasal.src = "./assets/imgs/room1.png";
+
+})
+
+casalBtn.addEventListener("click", () => {
+  solteiroBtn.classList.remove("selected");
+  casalBtn.classList.add("selected");
+
+  v1.textContent = "R$ 120,00";
+  v2.textContent = "R$ 140,00";
+
+  imgSolteiro.src = "./assets/imgs/room1.png";
+  imgCasal.src = "./assets/imgs/room2.png";
+});
